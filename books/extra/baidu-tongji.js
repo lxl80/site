8d2dc21e8f34d692df31b7b1494d90844cbd99ab
李小龙 <me@lixl.cn>
2020-04-13 18:11:41 +0800

    Site updated: 2020-04-13 18:11:39

---
+++ books/extra/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7e0bedc93e9b2cc20cfa835875bef2d6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
